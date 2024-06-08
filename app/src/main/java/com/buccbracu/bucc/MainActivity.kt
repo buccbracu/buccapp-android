603915858e257f6a8eee847db835996c142eca16
NilAdi <niloysahaaditya9876@gmail.com>
2024-06-08 23:40:05 +0600

    Feat : Updated

--- app/src/main/java/com/buccbracu/bucc/MainActivity.kt
+++ app/src/main/java/com/buccbracu/bucc/MainActivity.kt
@@ -35,6 +35,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+    var Hyper : String ="Hyperloop"
 }
 
 @Preview(showBackground = true)

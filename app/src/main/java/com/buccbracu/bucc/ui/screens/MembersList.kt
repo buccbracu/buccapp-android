77053d378e088bba43ecef6f30c278cb537db300
NilAdi2003 <154037919+NilAdi2003@users.noreply.github.com>
2024-06-08 18:42:44 +0600

    Update MembersList.kt

--- app/src/main/java/com/buccbracu/bucc/ui/screens/MembersList.kt
+++ app/src/main/java/com/buccbracu/bucc/ui/screens/MembersList.kt
@@ -1,51 +1,87 @@
 package com.buccbracu.bucc.ui.screens
 
-import android.media.Image
+import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.DoubleArrow
+import androidx.compose.material.icons.rounded.Group
+import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-// import androidx.compose.ui.tooling.preview.Preview
-import com.buccbracu.bucc.R
 import androidx.compose.ui.unit.dp
+import com.buccbracu.bucc.R
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun Memberslist(){
-    Column (
-            Modifier.verticalScroll(rememberScrollState()),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center)
-    {
-        for(i in 1..15){
-            MemberlistCard(ProfileImg = "Itachi", ProfileName = "Itachi Uchiha", ActiveTM = "$i hrs ago")
+    Scaffold(
+        topBar = {
+            Card(modifier = Modifier
+                .height(56.dp)
+                .padding(horizontal = 5.49.dp)
+
+                ) {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight()
+                        .padding(20.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(text = "All Members")
+                    Icon(imageVector = Icons.Rounded.Group, contentDescription = null)
+                }
+            }
+        }
+    ){
+        Column(
+            modifier = Modifier.verticalScroll(rememberScrollState()),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            for(i in 1..15){ MembersI()}
         }
     }
 }
 
 @Composable
-fun MemberlistCard(ProfileImg:String,ProfileName:String,ActiveTM:String){
-    Card() {
-        Row (horizontalArrangement = Arrangement.SpaceEvenly,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp)){
+fun MembersI(){
+    Card(modifier = Modifier
+            .height(80.dp)
+            .fillMaxWidth()
+            .padding(10.dp)) {
+        Row(modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically) {
             Image(painter = painterResource(id = R.drawable.itachi),
-                contentDescription = ProfileName,
+                contentDescription = null ,
                 modifier = Modifier
-                    .size(50.dp)
+                    .padding(horizontal = 5.dp)
+                    .size(40.dp)
                     .clip(CircleShape)
                     .border(
                         width = 2.dp,
@@ -53,11 +89,12 @@ fun MemberlistCard(ProfileImg:String,ProfileName:String,ActiveTM:String){
                         shape = CircleShape
                     ),
                 contentScale = ContentScale.Crop
-                )
-            Text(text = ProfileName)
-            Text(text = ActiveTM)
+            )
+            Text(text = "Itachi Uchiha")
+            Icon(imageVector = Icons.Rounded.DoubleArrow ,
+                contentDescription = null,
+                modifier = Modifier.padding(horizontal = 5.dp))
         }
     }
 }
-
 
